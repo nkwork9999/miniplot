dd42de1da75aaaf7987e7eb43c5d5379bcaf189d
nk <nkwork1234@gmail.com>
2025-10-03 00:43:59 +0900

    fix📊

--- src/miniplot_extension.cpp
+++ src/miniplot_extension.cpp
@@ -45,7 +45,7 @@ static void *rust_lib_handle = nullptr;
 inline void MiniplotTestFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &name_vector = args.data[0];
 	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Miniplot " + name.GetString() + " 📊");
+		return StringVector::AddString(result, "Miniplot " + name.GetString() + " 🐥"); 
 	});
 }
 
